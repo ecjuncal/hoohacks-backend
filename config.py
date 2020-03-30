ad34147553058c65f835d99c0c3722e61890a211
Evan Juncal <35431775+ecjuncal@users.noreply.github.com>
2020-03-29 20:11:56 -0400

    No more cloud build

--- config.py
+++ config.py
@@ -1,14 +1,11 @@
 import os
-from google.cloud import datastore
-import logging
+from dotenv import load_dotenv
 
 class Config:
     # General Config
     TESTING = True
     DEBUG = True
 
-    client = datastore.Client()
-    key = client.key('Database', 5634161670881280)
-    logging.info(client.get(key))
-    SQLALCHEMY_DATABASE_URI = client.get(key)
+    load_dotenv()
+    SQLALCHEMY_DATABASE_URI = os.environ.get('LOCAL_URI')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
